eca5c68c9fdec21a5ef3afe03b60cca24ab4e207
FabianCochonPro <167713089+FabianCochonPro@users.noreply.github.com>
2024-08-28 07:19:42 -0500

    Update 2.9.cpp

--- 2.9.cpp
+++ 2.9.cpp
@@ -1,1 +1,15 @@
 //2.9
+
+#include <iostream>
+int main()
+{
+  std::cout<<"Welcome to the maximal heart rate calculator";
+  int a=0;
+  int b=0;
+  int c=0;
+  //The program calculates the maximal heart of an individual 
+  std::cin>>a;
+  std::cin>>b;
+  std::cin>>c;
+  return 0;
+}
