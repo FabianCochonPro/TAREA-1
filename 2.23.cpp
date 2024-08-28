ba19c6d56bc7e9cf1d3e3da54e911336565a6508
FabianCochonPro <167713089+FabianCochonPro@users.noreply.github.com>
2024-08-28 07:44:58 -0500

    Create 2.23.cpp

---
+++ 2.23.cpp
@@ -0,0 +1,42 @@
+//2.23
+
+
+#include <iostream>
+int main()
+{
+  int a,b,c,d,e=0;
+  std::cout<<"Input 5 integers: ";
+  std::cin>>a>>b>>c>>d>>e;
+  
+  
+  int largest = a;
+  if (b > largest) {
+    largest = b;
+  }
+  if (c > largest) {
+    largest = c;
+  }
+  if (d > largest) {
+    largest = e;
+  }
+  if (e > largest) {
+    largest = e;
+  }
+  int smallest = a;
+  if (b < smallest) {
+    smallest = b;
+  }
+  if (c < smallest) {
+    smallest = c;
+  }
+  if (d < smallest) {
+    smallest = d;
+  }
+  if (e < smallest) {
+    smallest = e;
+  }  
+  std::cout << "Largest is " << largest << std::endl;
+  std::cout << "Smallest is " << smallest << std::endl;
+      
+      return 0;
+}
